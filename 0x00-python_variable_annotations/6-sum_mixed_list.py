ab35f8ba3e3cd9e4d451a2309dd2e3d74e680e98
iheb1919 <ihebmejri14@gmail.com>
2021-03-15 17:13:18 +0100

    trry

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import List, Union
 """
 sum_mixed_list annotated function
 """
 
 
-def sum_mixed_list(mxd_lst: List[int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     sum_mixed_list return sum of list
     """

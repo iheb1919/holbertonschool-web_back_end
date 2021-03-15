971e3557ddcfad4fcc03b004a929c64048c41639
iheb1919 <ihebmejri14@gmail.com>
2021-03-15 17:19:29 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ to_kv annotated function
 """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     to_kv function creat a tuple of float and str

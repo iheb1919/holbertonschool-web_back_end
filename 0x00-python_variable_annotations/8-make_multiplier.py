889d1b3dc4600047bcbc2f44429d184d9570061e
iheb1919 <ihebmejri14@gmail.com>
2021-03-15 17:16:44 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,7 @@ make_multiplier annotated function
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     make_multiplier functin return multplies by mu;plier

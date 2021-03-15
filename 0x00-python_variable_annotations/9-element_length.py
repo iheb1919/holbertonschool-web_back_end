5e28e436f42944b1dd10847e7a524356d77e1353
iheb1919 <ihebmejri14@gmail.com>
2021-03-15 17:15:37 +0100

    trry

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,7 +2,7 @@
 """
 annotated a function
 """
-from typing import *
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:

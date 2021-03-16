8949bc6810dbb7f89c1c36852749a243bec1c8fc
iheb1919 <ihebmejri14@gmail.com>
2021-03-16 16:01:05 +0100

    fix annotation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,12 +8,12 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """
     function with integers n and max_delay as arguments that measures
     the total execution time for wait_n(n, max_delay)
     """
-    start = time.time()
+    start: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end = time.time()
+    end: float = time.time()
     return (end - start) / n
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,11 @@
 """
 Tasks
 """
-
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.task:
     """
     function that takes an integer max_delay and returns a asyncio.Task
     """

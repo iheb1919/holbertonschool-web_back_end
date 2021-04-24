90339662f2fcaa05bb94610bf8974eb11d559bf8
root <root@LAPTOP-2RTLC8EF.localdomain>
2021-04-24 02:56:03 +0200

    try

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,9 @@ class BasicAuth(Auth):
     basicauth class
     """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        extract function
+        """
         if authorization_header is None or type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':

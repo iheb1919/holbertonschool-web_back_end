b825b4c2461c9a4d2cf69246e444bc0497e6d282
iheb1919 <ihebmejri14@gmail.com>
2021-04-24 02:00:32 +0200

    test

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+3. Auth class
 """
 from flask import request
 from typing import List, TypeVar
@@ -7,14 +8,21 @@ from typing import List, TypeVar
 
 class Auth():
     """
+    3. Auth class
     """
 
     def require_auth(self, path: str, excluded_path: List[str]) -> bool:
         """
+        require function
         """
-        if path is None or excluded_path is None:
+        if path is None or excluded_path is None or excluded_path == '':
             return True
-        return False 
+        if path[len(path) - 1] != '/':
+            path += '/'
+        for item in excluded_path:
+            if path == item:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """

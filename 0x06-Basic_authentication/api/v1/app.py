de4218155d67e43a9219490757850366d2308c7e
root <root@LAPTOP-2RTLC8EF.localdomain>
2021-04-24 02:03:39 +0200

    fix pycodestyle

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -55,6 +55,7 @@ def before_request() -> None:
     if not auth.current_user(request):
         abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")

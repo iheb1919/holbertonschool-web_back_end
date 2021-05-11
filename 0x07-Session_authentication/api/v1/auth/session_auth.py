866d8c598242332539b2b3ba9808a348021a3909
root <root@LAPTOP-2RTLC8EF.localdomain>
2021-05-11 11:22:09 +0200

    try

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """ 1.Empty session
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     class sessionauth
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        create_session
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        id_session = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[id_session] = user_id
+        return id_session
+        

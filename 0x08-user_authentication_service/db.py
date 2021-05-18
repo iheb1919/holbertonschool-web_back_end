77f4921560454b26d464c231c67a2aceb947f0ce
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-05-18 13:16:58 +0200

    fix

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -28,6 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
     def add_user(self, email: str, hashed_password: str):
         """
         add_user

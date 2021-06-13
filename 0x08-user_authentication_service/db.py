f1caae1d4541d894c8cdcb0b727543e1fa76f445
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-06-14 01:36:40 +0200

    try

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
-"""DB module
+"""
+Implement User class in DB
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
+from user import Base
+from user import User
 
 
 class DB:
-    """DB class DB module
+    """
+        DB class
+        create model to manage the Database
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+            Constructor
+            create the object BD
+        """
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """DB module Memoized session object
+    def _session(self):
+        """
+            Create if not exists a db session
+            Return the session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -31,7 +39,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """DB module add_user
+        """
+            Create an User and insert in Database
+            Retrun the new User
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
@@ -39,7 +49,9 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """DB module Find_user
+        """
+            Find an user in db
+            Return the user founded
         """
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:

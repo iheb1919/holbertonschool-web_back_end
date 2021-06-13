c28f1ba6fc90bf18c96dd105f008c9a58c08a1a7
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-06-14 01:35:01 +0200

    try

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -46,12 +46,4 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """DB module Update user
-        """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if key not in user.__dict__:
-                raise ValueError
-            setattr(user, key, value)
-        return None
+    

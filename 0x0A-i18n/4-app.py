d23013d02a51076cb9198fe610ea4b5c063e84d6
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-06-14 02:58:50 +0200

    try

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,44 +1,34 @@
 #!/usr/bin/env python3
-"""
-App FLASK
-"""
+""" This module creates a Flask app """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
 
 
-class Config:
-    """
-    Config class
-    """
+class Config(object):
+    """ Config class for babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
 app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Returns the index.html page """
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Best match language
-    """
+    """ Gets the locale from query string or request.accept_languages """
     locale = request.args.get("locale")
-    if locale in app.config['LANGUAGES']:
+    if locale is not None and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route("/", methods=["GET"], strict_slashes=False)
-def home():
-    """
-    home route
-    return: template
-    """
-    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
